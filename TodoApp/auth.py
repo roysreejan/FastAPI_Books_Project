867e2af6146cd26e57a8e1eb5303ed02678b1bd4
Sreejan Roy <sreejanroy01@gmail.com>
2022-06-11 16:58:24 +0600

    login a user and passing jwt within authectication barer header

--- TodoApp/auth.py
+++ TodoApp/auth.py
@@ -69,8 +69,9 @@ def create_access_token(username: str, user_id: int, expires_delta: Optional[tim
 async def get_current_user(token: str = Depends(oauth2_bearer)):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get["sub"]
-        user_id: int = payload.get["id"]
+        print(payload)
+        username: str = payload.get("sub")
+        user_id: int = payload.get("id")
         if username is None or user_id is None:
             raise get_user_exception()
         return {"username": username, "user_id": user_id}
--- TodoApp/main.py
+++ TodoApp/main.py
@@ -4,6 +4,7 @@ from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from pydantic import BaseModel, Field
 from typing import Optional
+from auth import get_current_user, get_user_exception
 
 
 app = FastAPI()
@@ -29,6 +30,14 @@ class Todo(BaseModel):
 @app.get("/")
 async def read_all(db: Session = Depends(get_db)):
     return db.query(models.Todos).all()
+
+
+@app.get("/todos/user")
+async def read_all_by_user(user: dict = Depends(get_current_user), db: Session = Depends(get_db)):
+    if user is None:
+        raise get_user_exception()
+    # query user detail from db
+    return db.query(models.Todos).filter(models.Todos.owner_id == user.get("user_id")).all()
 
 
 @app.get("/todo/{todo_id}")
